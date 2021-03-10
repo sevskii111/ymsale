29110ce89e85bbabdb199846670413445226ed1f
sevskii111 <sevskii111@gmail.com>
2021-03-10 20:41:40 +0300

    captchaaaa

--- auto_products_updater.js
+++ auto_products_updater.js
@@ -122,11 +122,12 @@ async function setupBrowser() {
     console.log("Captcha!");
     await captchaEl.screenshot({ path: `captcha.png` });
     const { solution, id } = await solveCaptcha();
+    console.log("Got soulution");
     await page.focus("input");
     await page.keyboard.type(solution);
     await page.click("button");
-    await page.waitForNavigation();
-    await sleep(5000);
+    //await page.waitForNavigation();
+    await sleep(20000);
     captchaEl = await page.$(".captcha__image img");
     console.log(solution);
     if (captchaEl) {
